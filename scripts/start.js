a3e3123478f77ff76ca73133f6adb8e432148f37
Alexander Neupokoev <admin@armavir.ru>
2021-06-29 21:41:35 +0300

    npde start

--- scripts/start.js
+++ scripts/start.js
@@ -1,11 +1,29 @@
+const path = require('path')
 const Hapi = require('@hapi/hapi')
+
 const port = process.env.PORT || 3000
+const FILES = /\.(js|js.map|woff|woff2|svg|bmp|jpg|jpeg|gif|png)(\?v=\d+\.\d+\.\d+)?$/
+const PATH = {
+    '/': 'index.html'
+}
 const init = async () => {
     const server = Hapi.server({
         port,
     })
 
     await server.register(require('@hapi/inert'))
+
+    server.route({
+        method: 'GET',
+        path: '/{path*}',
+        handler: (request, h) => {
+            if(FILES.test(request.path)) {
+                return h.file(path.join(process.cwd(), 'dist', request.path))
+            }
+            console.log(request.path)
+            return h.file(path.join(process.cwd(), 'dist', PATH[request.path]))
+        }
+    })
     await server.start()
     console.log('Server running %s', server.info.uri)
 }
